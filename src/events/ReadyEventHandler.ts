390bd6bae5f115c9badaf86d4f391543b68496a0
chew01 <chewjh19@gmail.com>
2022-06-08 16:59:49 +0800

    Added initializer for giveaways in db

--- src/events/ReadyEventHandler.ts
+++ src/events/ReadyEventHandler.ts
@@ -3,6 +3,7 @@ import BotEventHandler from '../types/BotEventHandler';
 import Logger from '../services/Logger';
 import type ExtendedClient from '../ExtendedClient';
 import Config from '../Config';
+import GiveawayManager from '../modules/GiveawayManager';
 
 class ReadyEventHandler extends BotEventHandler {
   name = 'ready';
@@ -13,6 +14,7 @@ class ReadyEventHandler extends BotEventHandler {
     if (!client.user) return;
     client.user.setActivity(Config.ACTIVITY_DESCRIPTION, { type: Config.ACTIVITY_TYPE });
     Logger.info(chalk.green.bold(`Ready! Bot logged in as ${client.user.tag}`));
+    GiveawayManager.restore();
   }
 }
 
